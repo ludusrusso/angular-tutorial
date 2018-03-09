dfa90230244a769e02a776d8be19aa196ab1201c
Ludovico O. Russo <ludus.russo@gmail.com>
2018-03-09 12:24:33 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Counter';
+  cnt: number = 0;
+
+  ngOnInit () {
+    setInterval( () => this.cnt += 1, 1000);
+  }
+
+  resetTimer() {
+    this.cnt = 0;
+  }
 }
